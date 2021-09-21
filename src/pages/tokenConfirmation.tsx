1d9dfac8ea008cd86a301824e5f12ff3c437f24c
Victor-Buendia <victorbuendia03@gmail.com>
2021-09-21 11:53:59 -0300

    Fix delay from confirmation page
    Co-authored-by: Brian Pina <brian.pina2001@gmail.com>
    Co-authored-by: Arthur Matos <git.arthurmatos@gmail.com>

--- src/pages/tokenConfirmation.tsx
+++ src/pages/tokenConfirmation.tsx
@@ -5,23 +5,23 @@ import { useTimeoutFn } from 'react-use';
 
 export const TokenConfirmation = () => {
 
-    const { token } = useParams<{ token: string }>();
-    const history = useHistory();
+	const { token } = useParams<{ token: string }>();
+	const history = useHistory();
 
-    useEffect(() => {
-        const confirmAccount = async () => {
-            await fetch(`${process.env.REACT_APP_STUDYT_API_URL}/auth/confirmation/${token}`, {
-                method: "POST"
-            });
-        }
-        confirmAccount();
-    }, []);
+	useEffect(() => {
+		const confirmAccount = async () => {
+			await fetch(`${process.env.REACT_APP_STUDYT_API_URL}/auth/confirmation/${token}`, {
+				method: 'POST'
+			});
+		};
+		confirmAccount();
+	}, []);
     
-    useTimeoutFn(() => history.replace('/login'), 3000);
+	useTimeoutFn(() => history.replace('/login'), 1000);
 
-    return (
-        <Flex h="100vh" justifyContent="center" alignItems="center">
-            <Heading>Confirmado com sucesso</Heading>
-        </Flex>
-    );
-}
+	return (
+		<Flex h="100vh" justifyContent="center" alignItems="center">
+			<Heading>Confirmado com sucesso</Heading>
+		</Flex>
+	);
+};
